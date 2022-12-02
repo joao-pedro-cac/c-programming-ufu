4b34ffa279fd72e71f5326c7b3cd4e76c6922c7f
João Pedro <joaopedrocac@outlook.com>
2022-12-01 22:15:18 -0300

    Added quaisDiv.c

---
+++ lab3 03-12-2022/quaisDiv.c
@@ -0,0 +1,43 @@
+/*
+ * Arquivo: quaisDiv.c
+ * Autor: João Pedro Cavalcante Albuquerque Costa
+ * Matrícula: 12121ECP021
+ * Criado em: 24/11/2022
+ */
+
+#include <stdio.h>
+
+typedef unsigned int uint;
+
+int main() {
+	// Variáveis
+	uint N;
+	int ref;
+
+	printf("# de números: ");
+	scanf("%u", &N);
+
+	int nums[N];
+
+	printf("NUMS: ");
+	for (int i = 0; i < N; i++) {
+		scanf("%d", nums + i);
+	}
+
+	printf("REFERÊNCIA: ");
+	scanf("%d", &ref);
+
+	for (int i = 0; i < N; i++) {
+		printf("%d", *(nums + i));
+
+		if (ref % *(nums + i) == 0) {
+			printf("#");                              // Caso o número seja divisor, marca-o com um '#'
+		}
+
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return 0;
+}
