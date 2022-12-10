efce09fc18d59b74b547f1c09b8d683b23145f91
João Pedro <joaopedrocac@outlook.com>
2022-12-10 20:04:08 -0300

    Added contaCaracteres.c

--- lab4 08-12-2022/contaCaracteres.c
+++ lab4 08-12-2022/contaCaracteres.c
@@ -2,7 +2,7 @@
  * Arquivo: contaCaracteres.c
  * Autor: João Pedro Cavalcante Albuquerque Costa
  * Matrícula: 12121ECP021
- * Criado em: 09/12/2022
+ * Criado em: 10/12/2022
  */
 
 #include <stdio.h>
@@ -11,11 +11,26 @@
 typedef unsigned int uint;
 
 char* strinput();
-char* showLetters(char str[]);
-char* showDigits(char str[]);
-char* showOthers(char str[]);
+uint* countChars(char str[]);
+char isAlpha(char chr);
+char isDigit(char chr);
 
 int main() {
+	char *txt;
+	uint *amounts;
+
+	printf("TEXTO (sem diacríticos): ");
+	txt = strinput();
+
+	amounts = countChars(txt);
+
+	printf("\nLetras:  %u\n", amounts[0]);
+	printf("Dígitos: %u\n", amounts[1]);
+	printf("Outros:  %u\n", amounts[2]);
+
+	free(txt);
+	free(amounts);
+
 	return 0;
 }
 
@@ -37,4 +52,52 @@ char* strinput() {
 	txt[i] = '\0';
 
 	return txt;
+}
+
+uint* countChars(char str[]) {
+	/* 
+	 * Contagem de caracteres
+	 * Retorna um vetor com 3 elementos:
+	 *     Elemento 1: Quantidade de letras
+	 *     Elemento 2: Quantidade de algarismos
+	 *     Elemento 3: Quantidade de caracteres não alfanuméricos
+	 */
+
+	uint *counts = (uint*) calloc(3, sizeof(int));
+	uint i = 0;
+	char letter = str[i];
+
+	while (letter) {
+		if (isAlpha(letter))
+			counts[0]++;
+		else if (isDigit(letter))
+			counts[1]++;
+		else
+			counts[2]++;
+
+		i++;
+		letter = str[i];
+	}
+
+	return counts;
+}
+
+
+char isAlpha(char chr) {
+	// Verifica se o caractere é uma letra
+
+	if (chr >= 'A' && chr <= 'Z')
+		return 1;
+	if (chr >= 'a' && chr <= 'z')
+		return 1;
+	return 0;
+}
+
+
+char isDigit(char chr) {
+	// Verifica se o caractere é um algarismo
+
+	if (chr >= '0' && chr <= '9')
+		return 1;
+	return 0;
 }
