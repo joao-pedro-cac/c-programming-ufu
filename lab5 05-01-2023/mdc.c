596af3440b9bc8aacc48d5a3e23d518e51494d84
João Pedro <joaopedrocac@outlook.com>
2023-01-06 11:31:35 -0300

    Added mdc.c

---
+++ lab5 05-01-2023/mdc.c
@@ -0,0 +1,29 @@
+/*
+ * Arquivo: mdc.c
+ * Autor: João Pedro Cavalcante Albuquerque Costa
+ * Matrícula: 12121ECP021
+ * Criado em: 5/1/2023
+ */
+
+#include <stdio.h>
+
+typedef unsigned int uint;
+
+uint mdc(uint a, uint b);
+
+int main() {
+	uint a, b, res;
+
+	printf("NUMS: ");
+	scanf("%u %u", &a, &b);
+
+	res = mdc(a, b);
+	printf("MDC = %u\n", res);
+
+	return 0;
+}
+
+uint mdc(uint a, uint b) {
+	if (!b) return a;
+	else return mdc(b, a % b);
+}
