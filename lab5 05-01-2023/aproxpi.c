0959897c5eb400df187ea9f882e1a52f669489af
João Pedro <joaopedrocac@outlook.com>
2023-01-06 15:31:16 -0300

    Added aproxpi.c

---
+++ lab5 05-01-2023/aproxpi.c
@@ -0,0 +1,31 @@
+/*
+ * Arquivo: aproxpi.c
+ * Autor: João Pedro Cavalcante Albuquerque Costa
+ * Matrícula: 12121ECP021
+ * Criado em: 05/01/2023
+ */
+
+#include <stdio.h>
+
+typedef unsigned int uint;
+
+double leibnizPi(uint n);
+
+int main() {
+	uint n;
+	double pi;
+
+	printf("PRECISÃO: ");
+	scanf("%u", &n);
+
+	pi = leibnizPi(n);
+	printf("PI = %lf\n", pi);
+
+	return 0;
+}
+
+double leibnizPi(uint n) {
+	 if (!n) return 4;
+	 else if (n % 2) return leibnizPi(n - 1) - 4.0 / (2 * n + 1);
+	 else return leibnizPi(n - 1) + 4.0 / (2 * n + 1);
+}
